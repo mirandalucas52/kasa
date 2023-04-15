95ea376f86748f985bdf9f7151d14c9b860d6c9a
Lucas Miranda <mirandalucas52@gmail.com>
2023-04-15 20:21:22 +0200

    ajout fetch

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,29 @@
 import "./Home.css";
 import Banner from "../../components/Banner/index";
 import Card from "../../components/Card/index";
-import Datas from "../../datas/logements.json";
 import BannerImage from "../../Images/banner_img.jpg";
+import { useEffect, useState } from "react";
 
 function Accueil() {
-    const items = Datas.map((data) => <Card key={data.id} {...data} />);
+    const [datas, setDatas] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("logements.json");
+            const data = await response.json();
+            setDatas(data);
+        };
+        fetchData();
+    }, []);
 
     return (
         <div className="accueil">
             <Banner displayText={true} imageSrc={BannerImage} />
-            <div className="gallery">{items}</div>
+            <div className="gallery">
+                {datas.map((data) => (
+                    <Card key={data.id} {...data} />
+                ))}
+            </div>
         </div>
     );
 }
